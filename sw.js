5421d0c52adfb01767a4f4577328e12cd35a9730
ADMFoundation <encouragingeducationproject@gmail.com>
2025-03-17 02:51:12 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -13,34 +13,48 @@ const urlsToCache = [
 // Install Service Worker
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      console.log("Opened cache");
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log("Opened cache");
+        return cache.addAll(urlsToCache)
+          .then(() => {
+            console.log("All resources cached successfully");
+          })
+          .catch((error) => {
+            console.error("Failed to cache all resources:", error);
+            // Optionally: You might decide to delete the cache here if caching failed completely.
+            // return caches.delete(CACHE_NAME);
+          });
+      })
+      .catch((error) => {
+        console.error("Failed to open cache:", error);
+      })
   );
 });
 
 // Fetch resources from cache
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
   );
 });
 
 // Activate Service Worker
 self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cache) => {
-          if (cache !== CACHE_NAME) {
-            console.log("Deleting old cache:", cache);
-            return caches.delete(cache);
-          }
-        })
-      );
-    })
+    caches.keys()
+      .then((cacheNames) => {
+        return Promise.all(
+          cacheNames.map((cache) => {
+            if (cache !== CACHE_NAME) {
+              console.log("Deleting old cache:", cache);
+              return caches.delete(cache);
+            }
+          })
+        );
+      })
   );
 });
